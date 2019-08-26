f27786433817bab27ba219a7ebf3b3402d87422a
Greg-tts <gtesto@techtalentsouth.com>
2019-08-26 14:09:02 -0700

    added active state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ class App extends React.Component{
   constructor(){
     super();
     this.state={
-
+      box1:false,
+      box2:false,
+      box3:false,
+      box4:false
     }
   }
   // // The HTML way
@@ -15,17 +18,20 @@ class App extends React.Component{
   // }
 
   handleClick=(id)=>{
-    console.log(id);
+    let currentBox = "box"+id;
+    let currentState = this.state[currentBox];
+    console.log(currentBox);
+    this.setState({[currentBox]: !currentState});
   }
   render(){
     return(
       <div>
         <h1>Simon</h1>
         <div id="boxWrapper">
-          <Box handleClick={this.handleClick} id={1} color={{num1:255, num2:0, num3:0}}/>
-          <Box handleClick={this.handleClick} id={2} color={{num1:0, num2:255, num3:0}}/>
-          <Box handleClick={this.handleClick} id={3} color={{num1:0, num2:0, num3:255}}/>
-          <Box handleClick={this.handleClick} id={4} color={{num1:255, num2:255, num3:0}}/>
+          <Box active={this.state.box1} handleClick={this.handleClick} id={1} color={{num1:255, num2:0, num3:0}}/>
+          <Box active={this.state.box2} handleClick={this.handleClick} id={2} color={{num1:0, num2:255, num3:0}}/>
+          <Box active={this.state.box3} handleClick={this.handleClick} id={3} color={{num1:0, num2:0, num3:255}}/>
+          <Box active={this.state.box4} handleClick={this.handleClick} id={4} color={{num1:255, num2:255, num3:0}}/>
         </div>
       </div>
     )
@@ -33,8 +39,9 @@ class App extends React.Component{
 }
 
 const Box = (props) =>{
+  let opacity = props.active ? ".75" : ".25";
   let boxStyle = {
-    backgroundColor:"rgba("+ props.color.num1 + "," + props.color.num2 + "," + props.color.num3 + ", .25)"
+    backgroundColor:"rgba("+ props.color.num1 + "," + props.color.num2 + "," + props.color.num3 + "," + opacity + ")"
   }
 
   return(
