c724ebf3a501fcb80e6775a6da7e9ca4eb38499c
Greg-tts <gtesto@techtalentsouth.com>
2019-08-26 11:47:33 -0700

    Extracted handleClick from component

--- src/App.js
+++ src/App.js
@@ -9,15 +9,18 @@ class App extends React.Component{
 
     }
   }
+  handleClick=()=>{
+    console.log("trigger");
+  }
   render(){
     return(
       <div>
         <h1>Simon</h1>
         <div id="boxWrapper">
-          <Box id={1} color={{num1:255, num2:0, num3:0}}/>
-          <Box id={2} color={{num1:0, num2:255, num3:0}}/>
-          <Box id={3} color={{num1:0, num2:0, num3:255}}/>
-          <Box id={4} color={{num1:255, num2:255, num3:0}}/>
+          <Box handleClick={this.handleClick} id={1} color={{num1:255, num2:0, num3:0}}/>
+          <Box handleClick={this.handleClick} id={2} color={{num1:0, num2:255, num3:0}}/>
+          <Box handleClick={this.handleClick} id={3} color={{num1:0, num2:0, num3:255}}/>
+          <Box handleClick={this.handleClick} id={4} color={{num1:255, num2:255, num3:0}}/>
         </div>
       </div>
     )
@@ -25,12 +28,11 @@ class App extends React.Component{
 }
 
 const Box = (props) =>{
-  console.log(props);
   let boxStyle = {
     backgroundColor:"rgba("+ props.color.num1 + "," + props.color.num2 + "," + props.color.num3 + ", .25)"
   }
   return(
-    <div onClick={()=>console.log("trigger")} className="boxStyling" style={boxStyle}>box {props.id}</div>
+    <div onClick={props.handleClick} className="boxStyling" style={boxStyle}>box {props.id}</div>
   )
 }
 
