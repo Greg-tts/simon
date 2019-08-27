9cc6887fe7b2f7cdbfb8b741fb08db1812632278
Greg-tts <gtesto@techtalentsouth.com>
2019-08-27 10:57:58 -0700

    added start button and add randomInt func

--- src/App.js
+++ src/App.js
@@ -6,13 +6,30 @@ class App extends React.Component{
   constructor(){
     super();
     this.state={
-      compPattern:[1,2,3,2,4],
+      compPattern:[],
       userPattern:[],
       box1:false,
       box2:false,
       box3:false,
       box4:false
     }
+  }
+
+  getRandomInt=(min, max)=>{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  addRandomNum=()=>{
+    let randNum = this.getRandomInt(1,5);
+    this.setState({
+      compPattern: [...this.state.compPattern, randNum]
+    })
+  }
+
+  componentDidMount(){
+    this.addRandomNum();
   }
 
   showPattern=(currPat)=>{
@@ -27,10 +44,16 @@ class App extends React.Component{
   }
 
   handleClick=(id)=>{
+    this.setState({
+      userPattern: [...this.state.userPattern, id]
+    });
+  }
+  startFunc=()=>{
     let compPat = this.state.compPattern;
     this.showPattern(compPat);
   }
   render(){
+    console.log(this.state.compPattern, this.state.userPattern)
     return(
       <div>
         <h1>Simon</h1>
@@ -40,6 +63,7 @@ class App extends React.Component{
           <Box active={this.state.box4} handleClick={this.handleClick} id={4} color={{num1:255, num2:255, num3:0}}/>
           <Box active={this.state.box3} handleClick={this.handleClick} id={3} color={{num1:0, num2:0, num3:255}}/>
         </div>
+        <button onClick={this.startFunc}>Start Game</button>
       </div>
     )
   }
